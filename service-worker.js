8db287c16cf04ba527a8e18d700f15189ba24a2f
Yevgeniy <zleonenkov@gmail.com>
2019-08-19 18:08:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.7dbf6aaa7a20e8ca9d607e82d26cab7b.js"
 );
 
 self.addEventListener('message', (event) => {
